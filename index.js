85bf12bd3086d44f734a267a913d5614091aa7cc
Owen Dresner <owendrez@gmail.com>
2022-03-19 17:44:54 -0400

    commit tree e443211ab7caf0e46576db4bd788a2a3311edc6b

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats=["Milo","Otis","Garfield"];
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(name){
+    const cats2=[...cats, name]
+    return cats2
+}
+function prependCat(name){
+    const cats2=[name, ...cats]
+    return cats2
+}
+function removeLastCat(){
+    const cats2=[...cats.slice(0, cats.length-1)]
+    return cats2
+}
+function removeFirstCat(){
+    const cats2=[...cats.slice(1)]
+    return cats2
+}
